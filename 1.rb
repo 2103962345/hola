2472ad6499d64deb8785fa09f8c198a06ac6e273
2103962345 <alexbobylewa@gmail.com>
2020-04-28 01:31:27 -0700

    Update bat file

--- 1.rb
+++ 1.rb
@@ -1,2 +1,3 @@
-#!/usr/bin/ruby
+sleep 1
 10.times { |i| puts "hello #{i}" }
+sleep 1
--- build.ps1
+++ build.ps1
@@ -1,31 +1,6 @@
-[CmdletBinding()]
-Param(
-    [Parameter(Position=0,Mandatory=$false,ValueFromRemainingArguments=$true)]
-    [string[]]$BuildArguments
-)
-
 Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.PSVersion)"
 
-Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
-$PSScriptRoot = Split-Path $MyInvocation.MyCommand.Path -Parent
+#Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
-###########################################################################
-# CONFIGURATION
-###########################################################################
-$ruby = "C:\Ruby27-x64\bin\ruby.exe C:\Users\Administrator\Downloads\hola\2.rb"
-#$BuildProjectFile = "C:\Users\Administrator\Downloads\hola\1.rb"
-$cmd = "C:\Windows\System32\cmd.exe"
-
-###########################################################################
-# EXECUTION
-###########################################################################
-
-function ExecSafe([scriptblock] $cmd) {
-    & $cmd
-    if ($LASTEXITCODE) { exit $LASTEXITCODE }
-}
-
-ExecSafe { & $ruby }
-#Write-Output $BuildProjectFile
-#& $BuildProjectFile
-#Invoke-Expression -Command "cmd.exe /c $ruby $BuildProjectFile"
+Write-Host "Congratulations! Your first script executed successfully"
+Start-Process -FilePath ruby -ArgumentList 'C:\Users\Administrator\Downloads\hola\1.rb'
