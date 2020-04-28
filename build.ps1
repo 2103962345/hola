716928cce0a252f7bb64ba42d994ff62a6c4923c
2103962345 <alexbobylewa@gmail.com>
2020-04-27 23:40:24 -0700

    Update bat file

--- build.ps1
+++ build.ps1
@@ -21,7 +21,8 @@ $BuildProjectFile = "$PSScriptRoot\1.rb"
 
 function ExecSafe([scriptblock] $cmd) {
     & $cmd
-    #if ($LASTEXITCODE) { exit $LASTEXITCODE }
+    if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
 
-ExecSafe { & $ruby $BuildProjectFile }
+#ExecSafe { & $ruby $BuildProjectFile }
+& $ruby $BuildProjectFile
