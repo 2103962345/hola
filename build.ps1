338bd4995aac9a034c8565a94fdce87a2e2c13b1
2103962345 <2103962345@mail.ru>
2020-05-18 15:51:06 +0300

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -3,5 +3,5 @@ Write-Output "PowerShell $($PSVersionTable.PSEdition) version $($PSVersionTable.
 #Set-StrictMode -Version 2.0; $ErrorActionPreference = "Stop"; $ConfirmPreference = "None"; trap { exit 1 }
 
 Write-Host "Congratulations! Your first script executed successfully"
-Invoke-Expression "ruby $PSScriptRoot\1.rb"
-#Start-Process -FilePath ruby -ArgumentList "$PSScriptRoot\1.rb" -NoNewWindow
+#Invoke-Expression "ruby $PSScriptRoot\1.rb"
+Start-Process -FilePath ruby -ArgumentList "$PSScriptRoot\1.rb" -NoNewWindow
